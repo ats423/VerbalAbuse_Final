ccbcc6e921442cd3882ef3cf345032aac41b3d25
Sara Arango <sarangofc@gmail.com>
2016-05-17 18:00:43 -0400

    List of top 10 occurring words

--- occurringWords.py
+++ occurringWords.py
@@ -21,14 +21,20 @@ for twt in Tweets:
     for wrd in twt['text'].strip().split(' '):
         txt = wrd.strip().split('.')[0]
         if txt != 'https://t' and txt not in stop_words and txt not in keywords:
-            map_words[txt] = 1#.strip().split('@')[-1:]
+            #.strip().split('@')[-1:]
             #print txt (?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)
             
             is_user = re.match( r'(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)',txt )
             is_hashtag = re.match( r'(?<=^|(?<=[^a-zA-Z0-9-_\.]))#([A-Za-z]+[A-Za-z0-9]+)',txt )
             if  is_user == None and is_hashtag == None:
-                print txt
+                try:
+                    map_words[txt] += 1
+                except KeyError:
+                    map_words[txt] = 1
 
+map_words.keys()
 
-reduce_words = set(map_words)
+import operator
 
+with open('./input_files/twtCount-words.json', 'w') as fp:
+   json.dump(dict( sorted(map_words.items(), key=operator.itemgetter(1))[-11:]), fp) 
